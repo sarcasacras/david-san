54a3d6451e6ec578bf7273886243ba29317d5f88
sarcasacras <dewaleguitar@gmail.com>
2023-03-26 17:20:05 +0200

    Жить стало лучше, жить стало веселей
    Удаление - мать учения

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
 const Artwork = require('./models/artwork');
+const methodOverride = require('method-override');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 mongoose.connect('mongodb://127.0.0.1:27017/david-san')
     .then(() => console.log('MongoDB connected'))
@@ -60,6 +62,17 @@ app.post('/artworks', (req, res) => {
             res.status(500).send('Error creating a new artwork');
         });
 });
+
+app.delete('/artworks/:id', (req, res) => {
+    Artwork.findByIdAndDelete(req.params.id)
+        .then(() => {
+            res.redirect('/artworks')
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send('Error deleting artwork');
+        })
+})
 
 app.listen(3000, () => {
     console.log('Server started on port 3000')
