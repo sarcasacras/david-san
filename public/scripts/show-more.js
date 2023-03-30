0cd1bc910fe9fed349787cb7429b1da2719d40b9
sarcasacras <dewaleguitar@gmail.com>
2023-03-30 11:05:52 +0200

    Show more update
    Добавил скрипт, который отображает на странице Artworks только первые 3 картины, а для отображения остальных, необходимо нажать на кнопку "Show more"

---
+++ public/scripts/show-more.js
@@ -0,0 +1,38 @@
+// По умолчанию кнопка спрятана
+const showMoreBtn = document.getElementById('show-more-btn');
+showMoreBtn.style.display = 'none';
+
+// Загружаем первые 3 картины
+let numArtworks = 3;
+let displayedArtworks = numArtworks;
+const artworkContainer = document.getElementById('aw-container');
+const artworks = document.querySelectorAll('.artwork');
+
+for (let i = 0; i < artworks.length; i++) {
+    if (i >= numArtworks) {
+        artworks[i].style.display = 'none';
+    }
+}
+
+// Добавляем EventListener на "Show More"
+showMoreBtn.addEventListener('click', () => {
+    numArtworks += 3;
+    for (let i = displayedArtworks; i < artworks.length; i++) {
+        if (i < numArtworks) {
+            artworks[i].style.display = 'block';
+        } else {
+            artworks[i].style.display = 'none';
+        }
+    }
+    displayedArtworks = numArtworks;
+
+    // Если все работы уже отображаются - убрать кнопку
+    if (displayedArtworks >= artworks.length) {
+        showMoreBtn.style.display = 'none';
+    }
+});
+
+// Если не все уже отображаются - показать кнопку
+if (artworks.length > numArtworks) {
+    showMoreBtn.style.display = 'block';
+}
